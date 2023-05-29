80d7d71618307c6362e09f898a51fbb47514fc63
HotDonkGG <kirillzhukov737@yandex.ru>
2023-05-30 03:18:30 +0700

    Исправление

--- src/main/java/filmorateapp/model/validation/ValidationService.java
+++ src/main/java/filmorateapp/model/validation/ValidationService.java
@@ -15,8 +15,7 @@ public class ValidationService {
             throw new ValidationException("Емейл юзера не может быть пустым и должен содержать @");
         } else if (user.getLogin() == null || user.getLogin().contains(" ")) {
             throw new ValidationException("Логин пользователя не может быть пустым или содержать пробелы");
-        } else if (user.getBirthday() != null && user.getBirthday().isAfter(ChronoLocalDate.from(LocalDateTime.
-                now()))) {
+        } else if (user.getBirthday() != null && user.getBirthday().isAfter(ChronoLocalDate.from(LocalDateTime.now()))) {
             throw new ValidationException("Пользователь не может быть рождён в будущем");
         }
     }
@@ -26,8 +25,7 @@ public class ValidationService {
             throw new ValidationException("фильм не может быть пустым");
         } else if (film.getDescription() != null && film.getDescription().length() > 200) {
             throw new ValidationException("Описание фильма более 200 символов");
-        } else if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(ChronoLocalDate.from(LocalDateTime.
-                of(1895, 12, 28, 0, 0)))) {
+        } else if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(ChronoLocalDate.from(LocalDateTime.of(1895, 12, 28, 0, 0)))) {
             throw new ValidationException("Дата релиза не может быть раньше чем 28/12/1895г.");
         } else if (film.getDuration() != 0 && film.getDuration() >= 0) {
             throw new ValidationException("Фильм должен иметь положительное значение");
