718ede0e78a9be2b1088e71788274b2dddb91de6
HotDonkGG <kirillzhukov737@yandex.ru>
2023-05-30 12:33:28 +0700

    Исправление

--- src/main/java/filmorateapp/model/Film.java
+++ src/main/java/filmorateapp/model/Film.java
@@ -1,22 +1,28 @@
 package filmorateapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 @AllArgsConstructor
 @Builder
 public class Film {
     private int id;
+    @NotBlank
+    @NonNull
     private String name;
+    @Size(max = 200)
     private String description;
+    @Past
     private LocalDate releaseDate;
+    @Positive
     private Long duration;
     private int like;
     private Set<Integer> likes;
--- src/main/java/filmorateapp/model/User.java
+++ src/main/java/filmorateapp/model/User.java
@@ -1,22 +1,24 @@
 package filmorateapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(force = true)
 @Builder
 public class User {
     private Long id;
+    @Email
+    @NonNull
     private String email;
     private String login;
     private String name;
+    @Past
     private LocalDate birthday;
     private Set<Long> friends;
 }
