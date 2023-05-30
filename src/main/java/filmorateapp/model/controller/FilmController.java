100eddf517f57ecda8d8a86f79142c5b0fde6641
HotDonkGG <kirillzhukov737@yandex.ru>
2023-05-30 14:33:26 +0700

    Исправление

--- src/main/java/filmorateapp/model/controller/FilmController.java
+++ src/main/java/filmorateapp/model/controller/FilmController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@RestController
+@RestControllerAdvice
 @Slf4j
 @RequestMapping(value = "/films", produces = "application/json")
 @RequiredArgsConstructor
--- src/main/java/filmorateapp/model/controller/UserController.java
+++ src/main/java/filmorateapp/model/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@RestControllerAdvice
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping(value = "/users", produces = "application/json")
